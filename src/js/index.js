236f55dd446fc6d8da4215f521269051fd0689fe
alexei <webdesfreelance@gmail.com>
2020-06-16 18:27:43 +0300

    save

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '.styles/style.scss';
+
+import { sum } from './sum.js';
+console.log( 'It"s fine working' );
+function main(){
+    console.log('this is main function')}
+    main();
+    sum(a,b)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+};
