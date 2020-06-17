f7bc9b5ea9b7a5df56bdb6abaf5876a8ed2c2015
alexei <webdesfreelance@gmail.com>
2020-06-17 11:19:19 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -9,24 +9,6 @@ module.exports = {
     filename: 'bundle.js'
   },
   
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: 'css-loader'
-          }, {
-            loader: 'sass-loader'
-          }],
-          fallback: 'style-loader'
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-     fileName: 'styles.css'
-    })
-  ]
+  
+
 };
