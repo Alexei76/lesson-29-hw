85357675a6abc0722d2d17925941552a54ba0949
alexei <webdesfreelance@gmail.com>
2020-06-17 11:42:47 +0300

    webpack-config changes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,24 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
-  
-  
+  module: {
+    rules: [
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        {
+          loader:MiniCssExtractPlugin.loader,
 
-};
+        },
+        'css-loader',
+        'sass-loader',
+
+      ],
+  },
+]
+},
+plugins: [new MiniCssExtractPlugin({
+  filename: 'styles.css'
+
+})],
+  
+  };
