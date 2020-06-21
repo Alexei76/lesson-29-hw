e32d545e974c2d2a5e29c3ccefa319ca48497d0e
alexei <webdesfreelance@gmail.com>
2020-06-21 12:01:57 +0300

    lodash

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 import '../styles/style.scss';
+import lodash from 'lodash-es';
 
 import { sum } from './sum.js';
+
 console.log( 'It"s fine working' );
+
 function main(){
-    console.log('this is main function')}
+    console.log('this is main function')
+}
     main();
     sum(a,b)
+    
+    let arr = [1,2,3];
+    let barr = [1,2,3];
+    console.log(lodash.isEqual(arr,barr))
