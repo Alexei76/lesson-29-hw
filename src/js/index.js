0aa65ece5dd3b65bf40f7a9673419ceeff45c1d4
alexei <webdesfreelance@gmail.com>
2020-06-21 15:24:28 +0300

    save

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 import '../styles/style.scss';
 import  './date.js';
-import { cities } from '../data/cityPhoto.js';
+import { citiers } from '../data/cityPhoto.js';
 
+function createCityDropdown(cities){
+    let select = document.createElement('select');
+    let target = document.querySelector('.locations');
+    
+    select.setAttribute('name','city-selector');
+    select.setAttribute('id','city-selector');
+    target.append(select);
+    createCityDropdown();
+}
