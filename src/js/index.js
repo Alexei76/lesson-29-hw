32cbbad26b08285562168da7787380bf6bf547d0
alexei <webdesfreelance@gmail.com>
2020-06-21 14:49:51 +0300

    data

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,12 @@ import '../styles/style.scss';
 
 import { isEqual } from 'lodash-es';
 
+import  './date.js';
+
 import { cities } from '../data/cityPhoto.js';
 
 import { sum } from './sum.js';
+
 
 console.log( 'It"s fine working' );
 
@@ -13,8 +16,17 @@ function main(){
 }
     main();
     sum(a,b)
+   
     
+   
     let arr = [1,2,3];
     let barr = [1,2,3];
     console.log(lodash.isEqual(arr,barr));
     console.log(cities);
+
+
+
+
+
+    
+   
