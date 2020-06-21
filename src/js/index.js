d9418d165afaa84d8ee92e3310b22c0667d0aa73
alexei <webdesfreelance@gmail.com>
2020-06-21 15:01:41 +0300

    clear_code

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,4 @@
 import '../styles/style.scss';
-
-import { isEqual } from 'lodash-es';
-
 import  './date.js';
-
 import { cities } from '../data/cityPhoto.js';
 
-import { sum } from './sum.js';
-
-
-console.log( 'It"s fine working' );
-
-function main(){
-    console.log('this is main function')
-}
-    main();
-    sum(a,b)
-   
-    
-   
-    let arr = [1,2,3];
-    let barr = [1,2,3];
-    console.log(lodash.isEqual(arr,barr));
-    console.log(cities);
-
-
-
-
-
-    
-   
